4c026b7e6b4c8d2a8649bdff0b0590b4910ffc0b
lhjd <hongjinlau@gmail.com>
2019-11-20 16:58:40 +0800

    Initial

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import tweetsData from './tweets';
+import Linkify from 'react-linkify';
+import * as moment from 'moment';
 
 class App extends React.Component {
+  
   render() {
+    let tweets = tweetsData.map(tweet => {
+
+      let img_url = null;
+      if (tweet.entities.media) {
+        img_url = tweet.entities.media[0].media_url;
+      }
+
+      var currentDateTime = moment();
+      var tweetCreatedDateTime = moment(tweet.created_at);
+      var hours = currentDateTime.diff(tweetCreatedDateTime, 'hours') // 1
+
+      return (
+      <>
+        <p><img src={tweet.user.profile_image_url} alt=""/>{tweet.user.name.toUpperCase()}  @{tweet.user.screen_name}  {hours}h
+        </p>
+        <p><Linkify>{tweet.text}</Linkify></p>
+        <p><img src={img_url} alt="" width="100px"/></p>
+        <p>Retweet: {tweet.retweet_count}</p>
+        <p>Favorite: {tweet.favorite_count}</p>
+        <hr/>
+      </>); 
+    })
+
     return (
       <div>
-        tweets
+        {tweets}
       </div>
     );
   }
@@ -15,4 +42,4 @@ const element = document.getElementById('app');
 
 ReactDOM.render(<App />, element );//
 
-console.log("tweet react");
+// console.log("tweet react");
